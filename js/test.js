c2eab6e23721daec9eba84b10a2d196ca866ff99
hossein <php71mhsh@gmail.com>
2023-09-28 23:07:52 +0330

    first commit webdesign

---
+++ js/test.js
@@ -0,0 +1,160 @@
+<script>
+
+// var lightbox=$('<div id="graylightbox"></div>');
+// var image = $("<img>");
+// var caption = $("<p></p>");
+
+
+// lightbox.append(image);
+// lightbox.append(caption);
+// $("body").append(lightbox);
+
+// $(".mainlightbox img").click(function(event){
+//   event.preventDefault();
+//   var imageloc=$(this).attr("src");
+//   image.attr("src",imageloc);
+//   captionimage=$(this).attr("alt");
+//   caption.text(captionimage);
+//   lightbox.show("slow","linear");
+// });
+
+// lightbox.click(function(){
+//   $(this).hide("slow","linear");
+// });
+
+
+ </script>
+  <script  >
+    //  var imageinternet= document.getElementById("internet");
+    //   imageinternet.setAttribute('width','550');
+    //   imageinternet.setAttribute("alt","internetblueimage");
+    
+    
+    // console.log(imageinternet.getAttribute("height"));
+    
+      // var users = ["تهران","hadi","esmail","hossein","soheil","ali","hamed","کرج","اصفهان"];
+    
+      // var selectlist= document.getElementById("userlist");
+      //   var createoption=document.createElement("option");
+      //       selectlist.appendChild(createoption);
+     
+      // users.forEach(element=> {
+      //     var selectoption= document.getElementById("userlist").appendChild(document.createElement("option"));
+      //     selectoption.innerHTML=element;
+      // });
+      // assigment
+      // i+=2;   i=i+2;
+      // i-=2;   i=i-2;
+      // i*=2;   i=i*2;
+      // i/=2;   i=i/2;
+    
+      // **=  x**=y     x=x**y;
+      // for(var i=0; i<= 100 ; i+=2){
+      //   //  if(i%2 != 0){
+    
+      //   //  }
+      //         console.log(i);
+      // }
+    
+      
+        // function myfunction() {
+        //   document.getElementById("headingtwo").innerHTML=" متن کادر کپی شد";
+        // }
+    
+        // function functiontwo(){
+        //   document.getElementById("paragraph").innerHTML=" متن کادر پیست  شد";
+    
+        // }
+        // function jam(a,b){
+    
+        //      console.log(a+b); 
+        // }
+        
+        // jam(2,3);
+        // jam(5,9);
+    
+      //  var name = "ali";
+      //  var family ="shafipoor";  
+      // var age = 20;  
+      // var score = 15.7;  
+    
+     
+      // console.log(users[3]);
+      // console.log(cars[0]);
+      // console.log(cars[1]);
+      // console.log(cars[2]);
+      // console.log(cars[3]);
+      // console.log(cars[4]);
+      // console.log(cars[5]);
+    
+         
+     
+    
+    
+    //  if(name=="ali"){
+    //      document.getElementById("headingone").innerHTML = name;
+    //      document.getElementById("headingone").classList.add("morestyle");
+    //  }else{
+    //   document.getElementById("headingone").innerHTML = "hossein";
+    //      document.getElementById("headingone").classList.add("teststyle");
+    //  }
+      //  document.getElementById("headingone").innerHTML= name;
+      //  document.getElementById("paragraph").innerHTML= family;
+        // document.getElementById("headingone").classList.add("animation");
+      // console.log("firstname: " + name +" lastname: "+ family +" age is: "+ age + " score is: " +  score);
+      // var selectoption= document.getElementById("listeman").appendChild(document.createElement("option"));
+      //  selectoption.innerHTML=element;
+      // var imagePath= document.getElementById("albumimg").getAttribute("src");
+      
+     
+    //  function imagepathlisten (){
+    //   document.addEventListener('click', (e)=>{
+    //  if(e.target.tagName=="IMG"){
+    //   image=  e.target.getAttribute("src");
+    //  }
+    //  return image;
+    
+    // })
+    
+    //  }
+    //  console.log(imagepathlisten);
+    // var getImageName = function() {
+    //   document.onclick = function(e) {
+    //     if (e.target.tagName == 'IMG') {
+    //       var image = e.target.getAttribute("src");
+    //       alert(image);
+    //     }
+    //   }
+    // }
+    // var a="ali";
+     
+     
+    // function dohref(param){
+     
+    //   return param.getAttribute("href") ;
+       
+    // }
+     
+    // function jam(a,b){
+    //   console.log(a+b);
+    // }
+     
+    // /  document.getElementById("soheilbabae").addEventListener("mousemove",function(){
+    //   console.log("babai w3schools zaban asli kheyli bahtare");
+    //  });
+    // function lighboxfunction(param){
+    //   // این رویداد و یا تابع ویا فانکشن پریونت دیفالت برای کپچر کردن صفحه اس 
+    //   event.preventDefault();
+    //  var creatdivelement = document.createElement('div');
+    //   // document.body.appendChild(creatdivelement);
+    //   creatdivelement.setAttribute("id","graylightbox");
+    //   var createimage=document.createElement('img');
+    //   creatdivelement.appendChild(createimage);
+    //   var imagelocation= param.getAttribute('src');
+    //   // console.log(imagelocation);
+    //   createimage.setAttribute('src',imagelocation);
+    
+    
+    // }
+     
+     </script>
